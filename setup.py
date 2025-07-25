706b48c039c26f5e28f880b585c6644c469a0d72
Zsofia Baruwa <132607844+zb15@users.noreply.github.com>
2025-07-26 00:12:16 +0100

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="NetVizCorpy",
-    version="0.1.4",
+    version="0.1.5",
     packages=find_packages(),
     install_requires=[
         "pandas",
@@ -14,7 +14,7 @@ setup(
         "matplotlib",
         "colorsys",
         "collections",
-        
+
         # any other dependencies
     ],
 
